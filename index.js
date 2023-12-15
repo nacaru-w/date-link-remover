9cb5ed57b39861272f97776fab1c505e88d8f0b6
nacaru-w <ignaciocr92@gmail.com>
2023-12-15 11:06:46 +0100

    Adding initialization function

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const loadDependencies = (callback) => {
 }
 
 const initializeScript = () => {
+    if (document.readyState == 'complete') {
+        const portletLink = mw.util.addPortletLink('p-views', '', 'Eliminar enlaces de fechas', 'date-link-remover', 'Remove links from dates');
+        if (portletLink)
+            portletLink.addEventListener("click", (e) => {
 
+            })
+    }
 }
 
 loadDependencies(initializeScript());
