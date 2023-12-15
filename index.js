1a6a6e2eba65b101b88bdd1395f69f2b84871b84
nacaru-w <ignaciocr92@gmail.com>
2023-12-15 13:51:35 +0100

    Finishing main function up

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+//<nowiki>
 
 // These should be loaded to ensure that everything works properly
 const loadDependencies = (callback) => {
     mw.loader.using('mediawiki.api', 'mediawiki.util', 'mediawiki.user');
-    callback();
+    callback;
 }
 
 // Function that gets the content of a page
@@ -29,21 +29,45 @@ function getContent(pageName) {
 // Main script function
 const initializeScript = () => {
     const page = mw.config.get('wgPageName');
-    if (document.readyState == 'complete') {
+    const namespace = mw.config.get('wgNamespaceNumber');
+    if (document.readyState == 'complete' && (namespace == 0 || namespace == 104 || namespace == 2)) {
         const regex = /\[\[(31|30|[12]\d|0?[1-9]) de (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)\]\]|\[\[20(?:[0-1]\d|20[0-2][0-3])\]\]|\[\[(19\d\d)\]\]|\[\d{1,2}\]\]/;
         getContent(page).then((content) => {
             if (regex.test(content)) {
                 console.log("found a date with brackets");
                 const portletLink = mw.util.addPortletLink('p-views', '#', 'Eliminar enlaces de fechas', 'date-link-remover', 'Remove links from dates');
-                if (portletLink)
+                if (portletLink) {
                     portletLink.addEventListener("click", (e) => {
                         console.log('testing')
+                        const sanitizerRegex = /\[\[(\d{1,2} de (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|\d{1,4})\]\]/g;
+                        new mw.Api().edit(
+                            page,
+                            (revision) => {
+                                return {
+                                    text: revision.content.replace(sanitizerRegex, '$1'),
+                                    summary: 'Eliminando enlaces según [[WP:FECHASENLACES]]',
+                                    minor: false
+                                }
+                            }
+                        ).then(() => {
+                            setTimeout(() => {
+                                console.log('Reloading page');
+                                location.reload();
+                            }, 1000);
+                        }).catch((error) => {
+                            alert(`Se ha producido un error: ${error.message}`);
+                            setTimeout(() => {
+                                location.reload()
+                            }, 3000);
+                        })
                     })
+                }
+
             }
         })
-
-
     }
 }
 
 loadDependencies(initializeScript());
+
+//</nowiki>
