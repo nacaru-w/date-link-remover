e5c19ce9b5749cf407a839335d9c1f11460ed342
nacaru-w <ignaciocr92@gmail.com>
2023-12-18 15:34:51 +0100

    ,Trying yet another way to manage DOM load and initilisation of the script

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //<nowiki>
 
-// These should be loaded to ensure that everything works properly
+// These should be loaded to ensure that everything works properly, the callback function that loads the script will only be called once DOM has been loaded
 const loadDependencies = (callback) => {
     mw.loader.using('mediawiki.api', 'mediawiki.util', 'mediawiki.user');
-    callback;
+    if (document.readyState !== 'loading') {
+        callback;
+    } else {
+        document.addEventListener('DOMContentLoaded', callback)
+    }
 }
 
 // Function that gets the content of a page
@@ -33,46 +37,43 @@ const initializeScript = () => {
     // Same applies to the current namespace
     const namespace = mw.config.get('wgNamespaceNumber');
     console.log(document.readyState);
-    if (document.readyState == "complete") {
-        console.log(document.readyState);
-        if (namespace == 0 || namespace == 104 || namespace == 2) {
-            const regex = /\[\[((\d{1,2} de )?(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|(\d{1,4}|siglo(\s|&nbsp;)*\w+)((\s|&nbsp;)*(a|d)\.(\s|&nbsp;)*C\.)?)(\|[^\]]*)*\]\]/i;
-            getContent(page).then((content) => {
-                if (regex.test(content)) {
-                    console.log("found a date with brackets");
-                    // This will add the button to remove the square brackets from dates if it finds such occurence in an article
-                    const portletLink = mw.util.addPortletLink('p-views', '#', 'WP:ENLACESFECHAS', 'enlaces-fechas', 'Se han detectado enlaces en fechas, clic aquí para eliminarlos');
-                    if (portletLink) {
-                        portletLink.addEventListener("click", () => {
-                            const sanitizerRegex = new RegExp(regex, "gi");
-                            // Call mw API to carry out the edit 
-                            new mw.Api().edit(
-                                page,
-                                (revision) => {
-                                    return {
-                                        text: revision.content.replace(sanitizerRegex, '$1'),
-                                        summary: 'Eliminando enlaces según [[WP:ENLACESFECHAS]] mediante [[Usuario:Nacaru/date-link-remover.js|script]]',
-                                        minor: false
-                                    }
+    if (namespace == 0 || namespace == 104 || namespace == 2) {
+        const regex = /\[\[((\d{1,2} de )?(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|(\d{1,4}|siglo(\s|&nbsp;)*\w+)((\s|&nbsp;)*(a|d)\.(\s|&nbsp;)*C\.)?)(\|[^\]]*)*\]\]/i;
+        getContent(page).then((content) => {
+            if (regex.test(content)) {
+                console.log("found a date with brackets");
+                // This will add the button to remove the square brackets from dates if it finds such occurence in an article
+                const portletLink = mw.util.addPortletLink('p-views', '#', 'WP:ENLACESFECHAS', 'enlaces-fechas', 'Se han detectado enlaces en fechas, clic aquí para eliminarlos');
+                if (portletLink) {
+                    portletLink.addEventListener("click", () => {
+                        const sanitizerRegex = new RegExp(regex, "gi");
+                        // Call mw API to carry out the edit 
+                        new mw.Api().edit(
+                            page,
+                            (revision) => {
+                                return {
+                                    text: revision.content.replace(sanitizerRegex, '$1'),
+                                    summary: 'Eliminando enlaces según [[WP:ENLACESFECHAS]] mediante [[Usuario:Nacaru/date-link-remover.js|script]]',
+                                    minor: false
                                 }
-                                // Reload the page
-                            ).then(() => {
-                                setTimeout(() => {
-                                    console.log('Reloading page');
-                                    location.reload();
-                                }, 500);
-                                // Catch any execution errors
-                            }).catch((error) => {
-                                alert(`Se ha producido un error: ${error}`);
-                                setTimeout(() => {
-                                    location.reload();
-                                }, 3000);
-                            })
+                            }
+                            // Reload the page
+                        ).then(() => {
+                            setTimeout(() => {
+                                console.log('Reloading page');
+                                location.reload();
+                            }, 500);
+                            // Catch any execution errors
+                        }).catch((error) => {
+                            alert(`Se ha producido un error: ${error}`);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 3000);
                         })
-                    }
+                    })
                 }
-            })
-        }
+            }
+        })
     }
 }
 
