2e8291ba8093b50288b427aad666eb9eb761d0ab
nacaru-w <ignaciocr92@gmail.com>
2023-12-18 13:33:53 +0100

    Trying a different approach

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const initializeScript = () => {
     const page = mw.config.get('wgPageName');
     // Same applies to the current namespace
     const namespace = mw.config.get('wgNamespaceNumber');
-    window.addEventListener("load", () => {
+    window.onload = () => {
         console.log("page loaded so loading date link remover");
         if (namespace == 0 || namespace == 104 || namespace == 2) {
             const regex = /\[\[((\d{1,2} de )?(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|(\d{1,4}|siglo(\s|&nbsp;)*\w+)((\s|&nbsp;)*(a|d)\.(\s|&nbsp;)*C\.)?)(\|[^\]]*)*\]\]/i;
@@ -72,7 +72,7 @@ const initializeScript = () => {
                 }
             })
         }
-    })
+    }
 
 }
 
