f4ef4084bdaf85c78107ad05726f30ed027652fa
nacaru-w <ignaciocr92@gmail.com>
2023-12-30 04:23:19 +0100

    Building main script, still needs work on timeout

--- control panel/control-panel.js
+++ control panel/control-panel.js
@@ -21,13 +21,32 @@ const dateLinkeRemoverControlPanel = (() => {
         }
     }
 
+    function getContent(pageName) {
+        let params = {
+            action: 'query',
+            prop: 'revisions',
+            titles: pageName,
+            rvprop: 'content',
+            rvslots: 'main',
+            formatversion: '2',
+            format: 'json'
+        };
+
+        let apiPromise = new mw.Api().get(params).then(
+            ((data) => {
+                return data.query.pages[0].revisions[0].slots?.main?.content
+            })
+        );
+        return apiPromise;
+    }
+
     function genArticleList() {
         const params = {
             action: 'query',
             format: 'json',
             list: 'random',
             rnnamespace: '0|104',
-            rnlimit: '5'
+            rnlimit: '100'
         },
             api = new mw.Api();
 
@@ -41,6 +60,32 @@ const dateLinkeRemoverControlPanel = (() => {
             return finalList
         });
         return apiPromise
+    }
+
+    function makeRegexGlobal(expression) {
+        return new RegExp(expression, "gi");
+    }
+
+    // Regexes variables
+    let regex = /\[\[((?:\d{1,2}º? de )?(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|(?:lunes|martes|miércoles|jueves|viernes|sábado|domingo)|(?:(?:años?|década de)\s)?(?:[1-9]\d{0,3}|siglo(?:\s|&nbsp;)*\w+)(?:(?:\s|&nbsp;)*(?:a|d)\.(?:\s|&nbsp;)*C\.)?)\]\]/i;
+    let pipeRegex = /\[\[((?:\d{1,2}º? de )?(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)|(?:lunes|martes|miércoles|jueves|viernes|sábado|domingo)|(?:(?:años?|década de)\s)?(?:[1-9]\d{0,3}|siglo(?:\s|&nbsp;)*\w+)(?:(?:\s|&nbsp;)*(?:a|d)\.(?:\s|&nbsp;)*C\.)?)(?:\|([^\]]*))\]\]/i;
+    let templateRegex = /(\{\{(?:siglo|(?:Julgreg)?fecha)[^\}]+)(?:\|1|\|Link\s*=\s*(?:\"true\"|(?:s[ií]|pt)))\s*(\}\})/i;
+
+    function textReplacer(articleText, applyRegex, applyPipeRegex, applyTemplateRegex) {
+        let newText = articleText;
+        if (applyRegex) {
+            regex = makeRegexGlobal(regex);
+            newText = newText.replace(regex, "$1");
+        }
+        if (applyPipeRegex) {
+            pipeRegex = makeRegexGlobal(pipeRegex);
+            newText = newText.replace(pipeRegex, "$2");
+        }
+        if (applyTemplateRegex) {
+            templateRegex = makeRegexGlobal(templateRegex);
+            newText = newText.replace(templateRegex, "$1$2");
+        }
+        return newText;
     }
 
     function loadControlPanel() {
@@ -60,6 +105,7 @@ const dateLinkeRemoverControlPanel = (() => {
         form.append({
             type: 'submit',
             label: 'Aceptar',
+            id: 'createArray',
         });
 
         let result = form.render();
@@ -68,18 +114,71 @@ const dateLinkeRemoverControlPanel = (() => {
     }
 
     function submit() {
+        let articleList;
         console.log('submitted');
+        const submitButton = document.querySelector('button.submitButtonProxy')
+        submitButton.id = 'submitButton';
+        submitButton.style = 'margin-right: 1em;';
+        submitButton.setAttribute('disabled', '');
+        const initializeButton = document.createElement("button");
+        initializeButton.innerText = 'Iniciar';
+        document.querySelector('span.morebits-dialog-buttons').append(initializeButton);
         const box = document.getElementById('articlesBox');
         genArticleList().then((result) => {
-            const articleList = result;
+            articleList = result;
             for (let article of articleList) {
                 console.log(article);
                 let spanElement = document.createElement("span");
-                spanElement.id = "article-line"
-                spanElement.textContent = article + ' ';
+                spanElement.id = article
+                spanElement.textContent = article;
+                spanElement.style = 'margin-right: 0.25em;'
+                let button = document.createElement("button");
+                button.innerText = '❌';
+                button.type = 'button';
+                button.style = 'margin-right: 1.25em;'
+                button.addEventListener('click', () => {
+                    const index = articleList.indexOf(article);
+                    if (index !== -1) {
+                        articleList.splice(index, 1);
+                        spanElement.style = 'text-decoration: line-through;';
+                    }
+                })
 
                 box.appendChild(spanElement);
+                box.appendChild(button);
+
             }
+        }).then(() => {
+            initializeButton.addEventListener("click", () => {
+                for (let article of articleList) {
+                    getContent(article).then((content) => {
+                        if (regex.test(content) || pipeRegex.test(content) || templateRegex.test(content)) {
+                            const useRegex = regex.test(content);
+                            const usePipeRegex = pipeRegex.test(content);
+                            const useTemplateRegex = templateRegex.test(content);
+                            setTimeout(async () => {
+                                await new mw.Api().edit(
+                                    article,
+                                    (revision) => {
+                                        return {
+                                            text: textReplacer(revision.content, useRegex, usePipeRegex, useTemplateRegex),
+                                            summary: 'Eliminando enlaces según [[WP:ENLACESFECHAS]]',
+                                            minor: false
+                                        }
+                                    }
+                                )
+                            }, 12000);
+                            const htmlElement = document.getElementById(article);
+                            htmlElement.style.color = 'darkgreen';
+                        } else {
+                            const htmlElement = document.getElementById(article);
+                            htmlElement.style.color = 'darkgray';
+                        }
+                    })
+
+                }
+                console.log("terminado");
+            })
         })
     }
 
